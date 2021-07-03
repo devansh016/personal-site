4a6285ab9beb330a5bf4eba7c40e8b06cbe4a024
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-03 20:19:08 +0530

    UI improved

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Footer from './footer'
 import Header from './header'
+import {container} from './layout.module.css'
+import '../styles/main.css'
 
 const Layout = (props) => {
     return (
-        <div>
+        <div className = { container }>
             <Header />
             {props.children}
             <Footer />
