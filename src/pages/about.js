6a28540b94e2bc5a878dd4a056e27cf75cda68a8
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-03 23:32:27 +0530

    fixes

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
             <p>I'm Devansh - currently majoring "Computer Science and Engineering" at Jaypee University of Information Technology, Solan.</p>
             <p>I am a young and energetic professional, looking for challenging opportunities to grow my skills.</p>
             <p>My intellectual curiosity also drives me to be a lifelong learner, and so I'm seeking to connect with like-minded people to learn and grow more. And in the way won't mind working under someone to know how everything works. And as far as my specialties?</p>
-            My specialties:
+            <span>My specialties:</span>
             <ul>
                 <li>Backend Development </li>
                 <li>Analytical thinking</li>
@@ -18,7 +18,6 @@ const AboutPage = () => {
             </ul>
             <p>I enjoy traveling and eating my way around the world, staying fit, and talking about brain or hacking, so if you ever want to bounce ideas off of me, please feel free to contact me.</p>
             <p>Connect to know more :)</p>
-        
         </Layout>
     )
 }
