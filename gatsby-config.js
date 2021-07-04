33a540b77410f7c25a5e23a12ed48d3f7c8556cb
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-04 18:19:18 +0530

    added blog functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,28 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'Devansh Chaudhary',
+    author: 'Devansh Chaudhary'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      }
+    }
+  ],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
-const footer = () => {
+const Footer = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site{
+                siteMetadata{
+                    author
+                }
+            }
+        }
+    `)
+
     return (
         <footer>
-            <center>Created by Devansh Chaudhary, © 2021 All rights reserved.</center>
+            <center>Created by {data.site.siteMetadata.author}, © 2021 All rights reserved.</center>
         </footer>
     )
 }
 
-export default footer
+export default Footer
