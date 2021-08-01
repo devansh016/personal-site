00890817815d513d0847a77ef7c351281b54abc6
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-08-02 01:07:54 +0530

    cleaned code

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path')
-const app = express();
-const port = process.env.PORT || 80;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(port);
-console.log(`Listening at port ${port}`);
